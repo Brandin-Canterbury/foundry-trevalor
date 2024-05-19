7d5edd17ee4995213054dadd095146ef47a442c6
Brandin Canterbury <BCanterbury@firstkeyhomes.com>
2024-05-19 18:52:30 -0400

    Added manifest and download

--- scripts/module.js
+++ scripts/module.js
@@ -1,3 +1,4 @@
 ﻿Hooks.once("init", function(){
     CONFIG.DND5E.featureTypes.class.subtypes.sentinelAlignment = "Sentinel Alignment";
+    CONFIG.DND5E.featureTypes.class.subtypes.elementalWarriorAttunement = "Elemental Warrior Attunement";    
 });
